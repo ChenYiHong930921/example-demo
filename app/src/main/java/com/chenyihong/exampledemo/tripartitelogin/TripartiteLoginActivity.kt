8fdcab5db188afabc19e5a6620da5de411f3df2a
ChenYHong <858203471@qq.com>
2022-05-06 18:14:31 +0800

    feat(all): 添加三方登录示例
    1. 完成Meta login接入。

--- app/src/main/java/com/chenyihong/exampledemo/tripartitelogin/TripartiteLoginActivity.kt
+++ app/src/main/java/com/chenyihong/exampledemo/tripartitelogin/TripartiteLoginActivity.kt
@@ -1,7 +1,11 @@
 package com.chenyihong.exampledemo.tripartitelogin
 
 import android.app.Activity
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Base64.DEFAULT
+import android.util.Base64.encodeToString
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.IntentSenderRequest
@@ -10,12 +14,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.chenyihong.exampledemo.R
 import com.chenyihong.exampledemo.databinding.LayoutTripartiteLoginActivityBinding
+import com.facebook.*
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.google.android.gms.auth.api.identity.GetSignInIntentRequest
 import com.google.android.gms.auth.api.identity.Identity
-import com.google.android.gms.auth.api.identity.SignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.common.api.ApiException
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
 const val TAG = "TripartiteLogin"
 
@@ -43,16 +51,37 @@ class TripartiteLoginActivity : AppCompatActivity() {
         }
     }
 
+    private lateinit var metaCallbackManager: CallbackManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<LayoutTripartiteLoginActivityBinding>(this, R.layout.layout_tripartite_login_activity)
+
+        metaCallbackManager = CallbackManager.Factory.create()
+        LoginManager.getInstance().registerCallback(metaCallbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(result: LoginResult) {
+                Log.i(TAG, "Meta login success")
+
+                Log.i(TAG, "Meta login account info userId:" + result.accessToken.userId)
+                Log.i(TAG, "Meta login account info token:" + result.accessToken.token)
+                Log.i(TAG, "Meta login account info applicationId:" + result.accessToken.applicationId)
+            }
+
+            override fun onCancel() {
+                Log.i(TAG, "Meta login canceled")
+            }
+
+            override fun onError(error: FacebookException) {
+                Log.e(TAG, "Meta login failed error:${error.message}")
+            }
+        })
 
         binding.btnGoogleLogin.setOnClickListener {
             checkGoogleLoginAccount(false)
         }
 
         binding.btnGoogleLogout.setOnClickListener {
-            logout()
+            googleLogout()
         }
 
         binding.btnGoogleOneTapLogin.setOnClickListener {
@@ -60,7 +89,11 @@ class TripartiteLoginActivity : AppCompatActivity() {
         }
 
         binding.btnFacebookLogin.setOnClickListener {
+            metaLogin()
+        }
 
+        binding.btnFacebookLogout.setOnClickListener {
+            metaLogout()
         }
     }
 
@@ -68,7 +101,7 @@ class TripartiteLoginActivity : AppCompatActivity() {
         val lastLoginAccountInfo = GoogleSignIn.getLastSignedInAccount(this)
         if (lastLoginAccountInfo == null) {
             if (oneTapLogin) {
-                oneTapLogin()
+                googleOneTapLogin()
             } else {
                 googleLogin()
             }
@@ -98,7 +131,7 @@ class TripartiteLoginActivity : AppCompatActivity() {
             }
     }
 
-    private fun oneTapLogin() {
+    private fun googleOneTapLogin() {
         val beginSignInRequest = BeginSignInRequest.builder()
             .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
                 .setSupported(true)
@@ -122,7 +155,7 @@ class TripartiteLoginActivity : AppCompatActivity() {
             }
     }
 
-    private fun logout() {
+    private fun googleLogout() {
         Identity.getSignInClient(this)
             .signOut()
             .addOnSuccessListener {
@@ -131,11 +164,49 @@ class TripartiteLoginActivity : AppCompatActivity() {
             }
     }
 
+    private fun metaLogin() {
+        val currentAccessTokenActive = AccessToken.isCurrentAccessTokenActive()
+        Log.i(TAG, "Meta login current AccessToken active :$currentAccessTokenActive")
+        if (!currentAccessTokenActive) {
+            LoginManager.getInstance().logIn(this, metaCallbackManager, listOf("public_profile"))
+        }
+    }
 
+    private fun metaLogout() {
+        Log.i(TAG, "Meta call logout")
+        LoginManager.getInstance().logOut()
+    }
+
+    private fun checkKeyStoreHash() {
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                val info = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNING_CERTIFICATES)
+
+                val signingInfo = info.signingInfo
+                val apkContentsSigners = signingInfo.apkContentsSigners
+
+                for (signature in apkContentsSigners) {
+                    val md: MessageDigest = MessageDigest.getInstance("SHA")
+                    md.update(signature.toByteArray())
+                    val keyStoreHash = encodeToString(md.digest(), DEFAULT)
+                    Log.d(TAG, "KeyHash keyStoreHash:${keyStoreHash}")
+                }
+            }
+        } catch (e: PackageManager.NameNotFoundException) {
+            Log.d(TAG, "KeyHash error1:${e.message}")
+        } catch (e: NoSuchAlgorithmException) {
+            Log.d(TAG, "KeyHash error2:${e.message}")
+        }
+    }
 
     private fun showToast(message: String) {
         runOnUiThread {
             Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
         }
+    }
+
+    override fun onDestroy() {
+        LoginManager.getInstance().unregisterCallback(metaCallbackManager)
+        super.onDestroy()
     }
 }
