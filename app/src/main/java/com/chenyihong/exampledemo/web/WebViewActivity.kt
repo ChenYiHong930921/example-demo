d78e65f178e0b6f2ed33e8e35bf5f91ecc6b52db
ChenYHong <858203471@qq.com>
2022-07-24 12:01:03 +0800

    feat(WebView) 添加WebView支持打开新标签示例

--- app/src/main/java/com/chenyihong/exampledemo/web/WebViewActivity.kt
+++ app/src/main/java/com/chenyihong/exampledemo/web/WebViewActivity.kt
@@ -4,27 +4,26 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.graphics.Bitmap
 import android.os.Bundle
+import android.os.Message
 import android.util.Log
 import android.view.View
 import android.webkit.*
-import android.widget.ImageView
-import androidx.appcompat.widget.AppCompatButton
-import androidx.constraintlayout.widget.ConstraintLayout
+import android.widget.FrameLayout
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
-import androidx.core.widget.ContentLoadingProgressBar
+import androidx.databinding.DataBindingUtil
 import com.chenyihong.exampledemo.R
+import com.chenyihong.exampledemo.databinding.LayoutWebViewActivityBinding
 
 const val TAG = "WebsiteTest"
 
 class WebViewActivity : Activity() {
 
-    private var rootView: ConstraintLayout? = null
+    private lateinit var layoutWebViewActivityBinding: LayoutWebViewActivityBinding
 
-    private var webView: WebView? = null
-
-    private var progressBar: ContentLoadingProgressBar? = null
+    private var mainWebView: WebView? = null
+    private var newWebView: WebView? = null
 
     /**
      * JavaScript interface
@@ -42,26 +41,22 @@ class WebViewActivity : Activity() {
         }
     }
 
-    @SuppressLint("InflateParams")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        rootView = layoutInflater.inflate(R.layout.layout_web_view_activity, null) as ConstraintLayout
-        setContentView(rootView)
-
+        layoutWebViewActivityBinding = DataBindingUtil.setContentView(this, R.layout.layout_web_view_activity)
         val insetsController = WindowCompat.getInsetsController(window, window.decorView)
         insetsController.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         insetsController.hide(WindowInsetsCompat.Type.systemBars())
-
-        progressBar = findViewById(R.id.pb_web_load_progress)
-
-        findViewById<ImageView>(R.id.iv_back).setOnClickListener { finish() }
-
-        webView = findViewById(R.id.web_view)
-        initWebViewSetting(webView)
-
-        findViewById<AppCompatButton>(R.id.btn_android_call_js).setOnClickListener { webView?.loadUrl("javascript:androidCallJsWithParams(true)") }
-
-        webView?.loadUrl("file:///android_asset/index.html")
+        layoutWebViewActivityBinding.ivBack.setOnClickListener { onBackPressed() }
+        layoutWebViewActivityBinding.btnAndroidCallJs.setOnClickListener { mainWebView?.loadUrl("javascript:androidCallJsWithParams(true)") }
+        mainWebView = WebView(this)
+        mainWebView?.let {
+            it.layoutParams = FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT)
+            initWebViewSetting(it)
+            layoutWebViewActivityBinding.webViewContainer.addView(it)
+            Log.i(TAG, "--------------create main web-----------------")
+        }
+        mainWebView?.loadUrl("file:///android_asset/index_open_tab.html")
     }
 
     @SuppressLint("JavascriptInterface", "SetJavaScriptEnabled")
@@ -76,6 +71,7 @@ class WebViewActivity : Activity() {
             settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
             settings.javaScriptEnabled = true
             settings.javaScriptCanOpenWindowsAutomatically = true
+            settings.setSupportMultipleWindows(true)
 
             addJavascriptInterface(jsInteractive, "JsInteractive")
             webChromeClient = object : WebChromeClient() {
@@ -89,21 +85,51 @@ class WebViewActivity : Activity() {
                 override fun onProgressChanged(view: WebView, newProgress: Int) {
                     super.onProgressChanged(view, newProgress)
                     Log.i(TAG, "web load progress:$newProgress")
-                    progressBar?.run {
+                    layoutWebViewActivityBinding.pbWebLoadProgress.run {
                         post { progress = newProgress }
                         if (newProgress >= 100 && visibility == View.VISIBLE) {
                             postDelayed({ visibility = View.GONE }, 500)
                         }
                     }
                 }
+
+                override fun onCreateWindow(view: WebView?, isDialog: Boolean, isUserGesture: Boolean, resultMsg: Message?): Boolean {
+                    newWebView = WebView(this@WebViewActivity)
+                    newWebView?.let { newWeb ->
+                        newWeb.layoutParams = FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT)
+                        initWebViewSetting(newWeb)
+                        layoutWebViewActivityBinding.webViewContainer.addView(newWeb)
+                        Log.i(TAG, "--------------create new web-----------------")
+                        resultMsg?.run {
+                            val webViewTransport = obj as? WebView.WebViewTransport
+                            webViewTransport?.let { transport ->
+                                transport.webView = newWeb
+                            }
+                            sendToTarget()
+                        }
+                    }
+                    return true
+                }
+
+                override fun onCloseWindow(window: WebView?) {
+                    destroyNewWebView()
+                    super.onCloseWindow(window)
+                }
             }
             webViewClient = object : WebViewClient() {
                 override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
                     super.onPageStarted(view, url, favicon)
                     Log.i(TAG, "WebViewActivity onPageStarted url:$url")
-                    progressBar?.run {
+                    layoutWebViewActivityBinding.pbWebLoadProgress.run {
                         post { visibility = View.VISIBLE }
                     }
+                }
+
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    Log.i(TAG, "WebViewActivity onPageFinished view:$view url:$url")
+                    val message="mainWeb:$mainWebView||newWeb:$newWebView||currentWeb:$view"
+                    view?.loadUrl("javascript:androidCallJsWithParams(\"$message\")")
                 }
             }
         }
@@ -111,7 +137,7 @@ class WebViewActivity : Activity() {
 
     override fun onResume() {
         super.onResume()
-        webView?.run {
+        mainWebView?.run {
             onResume()
             resumeTimers()
         }
@@ -119,26 +145,46 @@ class WebViewActivity : Activity() {
 
     override fun onPause() {
         super.onPause()
-        webView?.run {
+        mainWebView?.run {
             onPause()
             pauseTimers()
         }
     }
 
     override fun onDestroy() {
-        webView?.loadDataWithBaseURL(null, "", "text/html", "utf-8", null)
-        webView?.clearHistory()
-        rootView?.removeView(webView)
-        webView?.destroy()
-        webView = null
+        destroyMainWebView()
+        destroyNewWebView()
         super.onDestroy()
     }
 
     override fun onBackPressed() {
-        if (webView?.canGoBack() == true) {
-            webView?.goBack()
-        } else {
-            super.onBackPressed()
+        when {
+            newWebView?.canGoBack() == true -> newWebView?.goBack()
+            newWebView != null -> destroyNewWebView()
+            mainWebView?.canGoBack() == true -> mainWebView?.goBack()
+            else -> super.onBackPressed()
         }
+    }
+
+    private fun destroyMainWebView() {
+        mainWebView?.run {
+            Log.i(TAG, "destroy mainWeb webView:$this")
+            clearHistory()
+            loadDataWithBaseURL(null, "", "text/html", "utf-8", null)
+            layoutWebViewActivityBinding.webViewContainer.removeView(this)
+            destroy()
+        }
+        mainWebView = null
+    }
+
+    private fun destroyNewWebView() {
+        newWebView?.run {
+            Log.i(TAG, "destroy newWeb webView:$this")
+            clearHistory()
+            loadDataWithBaseURL(null, "", "text/html", "utf-8", null)
+            layoutWebViewActivityBinding.webViewContainer.removeView(this)
+            destroy()
+        }
+        newWebView = null
     }
 }
