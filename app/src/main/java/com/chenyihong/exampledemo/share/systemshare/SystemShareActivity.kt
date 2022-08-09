f555164117e58f857ece06bed92249d1c1f4d83b
ChenYHong <858203471@qq.com>
2022-08-09 10:30:01 +0800

    feat(SystemShare): 增加多图分享和混合资源分享

--- app/src/main/java/com/chenyihong/exampledemo/share/systemshare/SystemShareActivity.kt
+++ app/src/main/java/com/chenyihong/exampledemo/share/systemshare/SystemShareActivity.kt
@@ -1,18 +1,23 @@
 package com.chenyihong.exampledemo.share.systemshare
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.chenyihong.exampledemo.R
 import com.chenyihong.exampledemo.databinding.LayoutSystemShareActivityBinding
+import com.chenyihong.exampledemo.resultapi.custom.PickMultiplePhotoContract
 import com.chenyihong.exampledemo.resultapi.custom.PickSinglePhotoContract
 import com.chenyihong.exampledemo.share.MimeType
 
 const val TAG = "SystemShare"
 
 class SystemShareActivity : AppCompatActivity() {
+
+    private val sharePhotoUri = ArrayList<Uri>()
+    private val shareVideoUrl = ArrayList<Uri>()
 
     private val pickPhoto = registerForActivityResult(PickSinglePhotoContract()) { uri ->
         if (uri != null) {
@@ -25,9 +30,20 @@ class SystemShareActivity : AppCompatActivity() {
             startActivity(shareIntent)
         }
     }
-
+    private val pickPhotos = registerForActivityResult(PickMultiplePhotoContract()) { uriList ->
+        if (uriList.isNotEmpty()) {
+            sharePhotoUri.addAll(uriList)
+            val photoShareIntent = Intent().apply {
+                action = Intent.ACTION_SEND_MULTIPLE
+                putParcelableArrayListExtra(Intent.EXTRA_STREAM, sharePhotoUri)
+                type = MimeType.IMAGE_ALL
+            }
+            startActivity(Intent.createChooser(photoShareIntent, "SharePhotoTitle"))
+        }
+    }
     private val pickVideo = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
         if (uri != null) {
+            shareVideoUrl.add(uri)
             val pictureShareIntent: Intent = Intent().apply {
                 action = Intent.ACTION_SEND
                 putExtra(Intent.EXTRA_STREAM, uri)
@@ -69,6 +85,22 @@ class SystemShareActivity : AppCompatActivity() {
         }
         binding.btnShareVideo.setOnClickListener {
             pickVideo.launch(MimeType.VIDEO_All)
+        }
+        binding.btnSharePictures.setOnClickListener {
+            pickPhotos.launch(null)
+        }
+        binding.btnShareMultipleMedium.setOnClickListener {
+            val mediumUris = ArrayList<Uri>()
+            mediumUris.addAll(sharePhotoUri)
+            mediumUris.addAll(shareVideoUrl)
+            if (mediumUris.isNotEmpty()) {
+                val photoShareIntent = Intent().apply {
+                    action = Intent.ACTION_SEND_MULTIPLE
+                    putParcelableArrayListExtra(Intent.EXTRA_STREAM, mediumUris)
+                    type = MimeType.ALL
+                }
+                startActivity(Intent.createChooser(photoShareIntent, "ShareMultipleMediumTitle"))
+            }
         }
     }
 }
