a899b889b037bc6bdb62380231a53762e4ffb6d0
ChenYHong <858203471@qq.com>
2023-02-21 17:50:57 +0800

    feat(Chrome Custom Tab): 完善Chrome Custom Tab示例
    1. 增加添加ActionButton代码。
    2. 增加添加菜单项代码。
    3. 增加预加载代码。

--- app/src/main/java/com/chenyihong/exampledemo/web/customtab/CustomTabExampleActivity.kt
+++ app/src/main/java/com/chenyihong/exampledemo/web/customtab/CustomTabExampleActivity.kt
@@ -1,11 +1,10 @@
 package com.chenyihong.exampledemo.web.customtab
 
 import android.annotation.SuppressLint
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Rect
+import android.graphics.*
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContract
@@ -20,7 +19,14 @@ import com.chenyihong.exampledemo.utils.DensityUtil
 import com.chenyihong.exampledemo.web.PARAMS_LINK_URL
 import com.chenyihong.exampledemo.web.WebViewActivity
 
+const val TAG = "CustomTabExampleTag"
+
 class CustomTabExampleActivity : BaseGestureDetectorActivity() {
+
+    private val ACTION_ID = "actionId"
+    private val ACTION_ID_SCAN = 1
+    private val ACTION_ID_COPY_LINK = 2
+    private val ACTION_ID_SEARCH = 3
 
     private var changeHeightByActivityResult = false
 
@@ -52,6 +58,10 @@ class CustomTabExampleActivity : BaseGestureDetectorActivity() {
         val binding: LayoutCustomTabActivityBinding = DataBindingUtil.setContentView(this, R.layout.layout_custom_tab_activity)
         binding.includeTitle.tvTitle.text = "Chrome Custom Tab"
         activityHeight = (resources.displayMetrics.heightPixels * 0.8).toInt()
+        binding.btnOpenSimpleTab.setOnClickListener {
+            checkCustomTabAvailable()
+            CustomTabHelper.openSimpleCustomTab(this, url)
+        }
         binding.btnChangeHeightFixed.setOnClickListener {
             checkCustomTabAvailable()
             changeHeightAdjustable = false
@@ -80,6 +90,31 @@ class CustomTabExampleActivity : BaseGestureDetectorActivity() {
             checkCustomTabAvailable()
             CustomTabHelper.openCustomTabWithCustomAnimations(this, url)
         }
+        binding.btnCustomActionButton.setOnClickListener {
+            checkCustomTabAvailable()
+            ContextCompat.getDrawable(this, R.drawable.icon_scan_32_black)?.let {
+                CustomTabHelper.openCustomTabWithCustomActionButton(this, url, toBitmap(it), "Scan", createPendingIntent(ACTION_ID_SCAN))
+            }
+        }
+        binding.btnCustomMenu.setOnClickListener {
+            checkCustomTabAvailable()
+            CustomTabHelper.openCustomTabWithCustomMenu(this, url, mapOf(
+                Pair("Copy Link", createPendingIntent(ACTION_ID_COPY_LINK)),
+                Pair("Search In App", createPendingIntent(ACTION_ID_SEARCH))
+            ))
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        intent?.run {
+            when (getIntExtra(ACTION_ID, -1)) {
+                ACTION_ID_SCAN -> showToast("click scan action button")
+                ACTION_ID_COPY_LINK -> showToast("click copy link menu item")
+                ACTION_ID_SEARCH -> showToast("click search menu item")
+                else -> showToast("Unknown action id")
+            }
+        }
     }
 
     private fun checkCustomTabAvailable() {
@@ -103,7 +138,7 @@ class CustomTabExampleActivity : BaseGestureDetectorActivity() {
         }
     }
 
-    private fun toBitmap(drawable: Drawable): Bitmap? {
+    private fun toBitmap(drawable: Drawable): Bitmap {
         val width = DensityUtil.dp2Px(24)
         val height = DensityUtil.dp2Px(24)
         val oldBounds = Rect(drawable.bounds)
@@ -112,5 +147,12 @@ class CustomTabExampleActivity : BaseGestureDetectorActivity() {
         drawable.draw(Canvas(bitmap))
         drawable.bounds = oldBounds
         return bitmap
+    }
+
+    private fun createPendingIntent(actionId: Int): PendingIntent {
+        val actionIntent = Intent(applicationContext, CustomTabExampleActivity::class.java).apply {
+            putExtra(ACTION_ID, actionId)
+        }
+        return PendingIntent.getActivity(applicationContext, this.hashCode() + actionId, actionIntent, PendingIntent.FLAG_IMMUTABLE)
     }
 }
--- app/src/main/java/com/chenyihong/exampledemo/web/customtab/CustomTabHelper.kt
+++ app/src/main/java/com/chenyihong/exampledemo/web/customtab/CustomTabHelper.kt
@@ -1,6 +1,7 @@
 package com.chenyihong.exampledemo.web.customtab
 
 import android.app.Activity
+import android.app.PendingIntent
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
@@ -9,22 +10,33 @@ import android.content.pm.ResolveInfo
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Build
+import android.os.Bundle
+import android.util.Log
 import androidx.annotation.ColorInt
 import androidx.browser.customtabs.CustomTabColorSchemeParams
 import androidx.browser.customtabs.CustomTabsClient
 import androidx.browser.customtabs.CustomTabsIntent
+import androidx.browser.customtabs.CustomTabsService
 import androidx.browser.customtabs.CustomTabsServiceConnection
+import androidx.browser.customtabs.CustomTabsSession
 import androidx.core.net.toUri
 import com.chenyihong.exampledemo.R
 
 object CustomTabHelper {
 
+    private val mayLaunchUrl = arrayListOf("https://go.minigame.vip/", "https://juejin.cn/", "https://www.baidu.com/")
+
     private var customTabAvailablePackageName: String = ""
     private var customTabsClient: CustomTabsClient? = null
+    private var customTabsSession: CustomTabsSession? = null
     private var customTabsServiceConnection: CustomTabsServiceConnection? = null
 
+    fun openSimpleCustomTab(context: Context, url: String) {
+        CustomTabsIntent.Builder(customTabsSession).build().launchUrl(context, url.toUri())
+    }
+
     fun openCustomTabWithInitialHeight(context: Context, url: String, activityHeight: Int = 0, radius: Int = 0, adjustable: Boolean = false) {
-        val customTabsIntentBuilder = CustomTabsIntent.Builder(customTabsClient?.newSession(null))
+        val customTabsIntentBuilder = CustomTabsIntent.Builder(customTabsSession)
         if (activityHeight != 0) {
             // 第二个参数配置预期行为
             // ACTIVITY_HEIGHT_ADJUSTABLE 用户可以手动调整视图高度
@@ -38,7 +50,7 @@ object CustomTabHelper {
     }
 
     fun openCustomTabWithCustomUI(context: Context, url: String, @ColorInt color: Int = 0, showTitle: Boolean = false, autoHide: Boolean = false, backIconPosition: Int = CustomTabsIntent.CLOSE_BUTTON_POSITION_START, backIcon: Bitmap? = null) {
-        val customTabsIntentBuilder = CustomTabsIntent.Builder(customTabsClient?.newSession(null))
+        val customTabsIntentBuilder = CustomTabsIntent.Builder(customTabsSession)
         if (color != 0) {
             // 配置背景颜色
             customTabsIntentBuilder.setDefaultColorSchemeParams(CustomTabColorSchemeParams.Builder()
@@ -59,9 +71,25 @@ object CustomTabHelper {
     }
 
     fun openCustomTabWithCustomAnimations(context: Context, url: String) {
-        val customTabsIntentBuilder = CustomTabsIntent.Builder(customTabsClient?.newSession(null))
+        val customTabsIntentBuilder = CustomTabsIntent.Builder(customTabsSession)
         customTabsIntentBuilder.setStartAnimations(context, R.anim.slide_in_right, R.anim.slide_out_left)
         customTabsIntentBuilder.setExitAnimations(context, android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+        customTabsIntentBuilder.build().launchUrl(context, url.toUri())
+    }
+
+    fun openCustomTabWithCustomActionButton(context: Context, url: String, actionButtonIcon: Bitmap, actionButtonLabel: String, pendingIntent: PendingIntent) {
+        val customTabsIntentBuilder = CustomTabsIntent.Builder(customTabsSession)
+        // 设置按钮的图标、文案以及点击图标时触发的PendingIntent
+        customTabsIntentBuilder.setActionButton(actionButtonIcon, actionButtonLabel, pendingIntent)
+        customTabsIntentBuilder.build().launchUrl(context, url.toUri())
+    }
+
+    fun openCustomTabWithCustomMenu(context: Context, url: String, menuItemParams: Map<String, PendingIntent>) {
+        val customTabsIntentBuilder = CustomTabsIntent.Builder(customTabsSession)
+        menuItemParams.entries.forEach {
+            // 设置菜单项的文案，以及点击菜单项时触发的PendingIntent
+            customTabsIntentBuilder.addMenuItem(it.key, it.value)
+        }
         customTabsIntentBuilder.build().launchUrl(context, url.toUri())
     }
 
@@ -81,7 +109,7 @@ object CustomTabHelper {
         val supportingCustomTabResolveInfo = ArrayList<ResolveInfo>()
         browsableResolverInfo.forEach {
             val serviceIntent = Intent().apply {
-                action = androidx.browser.customtabs.CustomTabsService.ACTION_CUSTOM_TABS_CONNECTION
+                action = CustomTabsService.ACTION_CUSTOM_TABS_CONNECTION
                 setPackage(it.activityInfo.packageName)
             }
             val customTabServiceResolverInfo = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
@@ -105,11 +133,31 @@ object CustomTabHelper {
             if (customTabsClient == null) {
                 customTabsServiceConnection = object : CustomTabsServiceConnection() {
                     override fun onCustomTabsServiceConnected(name: ComponentName, client: CustomTabsClient) {
+                        // 预热浏览器，返回是否成功
+                        val warmup = client.warmup(0)
+                        Log.i(TAG, "Custom Tab Client Warmup succeed:$warmup")
+                        customTabsSession = client.newSession(null)
+                        customTabsSession?.let {
+                            var mainUri: Uri? = null
+                            val otherLinkBundles = ArrayList<Bundle>()
+                            for ((index, url) in mayLaunchUrl.withIndex()) {
+                                if (index == 0) {
+                                    mainUri = url.toUri()
+                                } else {
+                                    otherLinkBundles.add(Bundle().apply { putParcelable(CustomTabsService.KEY_URL, url.toUri()) })
+                                }
+                            }
+                            // 配置可能会访问的网站
+                            // 设置一个主链接，可以通过Bundle List传入其他可能会打开的链接
+                            val setMayLaunchUrl = it.mayLaunchUrl(mainUri, null, otherLinkBundles)
+                            Log.i(TAG, "Custom Tab Session Set may launch url succeed:$setMayLaunchUrl")
+                        }
                         customTabsClient = client
                     }
 
                     override fun onServiceDisconnected(name: ComponentName?) {
                         customTabsClient = null
+                        customTabsSession = null
                     }
                 }
                 customTabsServiceConnection?.let {
@@ -122,6 +170,7 @@ object CustomTabHelper {
     fun unbindCustomTabsService(activity: Activity) {
         customTabsServiceConnection?.let { activity.unbindService(it) }
         customTabsClient = null
+        customTabsSession = null
         customTabsServiceConnection = null
     }
 }
