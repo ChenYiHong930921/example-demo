e9e07684ff2c4c78ceed1f7f6086db8121ea5911
ChenYHong <858203471@qq.com>
2023-03-21 14:45:20 +0800

    feat(TimeChange): 添加时间变化示例

--- app/src/main/java/com/chenyihong/exampledemo/androidapi/timechange/TimeChangeExample.kt
+++ app/src/main/java/com/chenyihong/exampledemo/androidapi/timechange/TimeChangeExample.kt
@@ -5,6 +5,9 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.SystemClock
 import android.util.Log
 import com.chenyihong.exampledemo.androidapi.gesturedetector.BaseGestureDetectorActivity
 import java.util.*
@@ -21,15 +24,36 @@ class TimeChangeExample : BaseGestureDetectorActivity() {
         }
     }
 
+    private var countDownTime = 0
+    private var onlineTime = 0
+
+    private val handler = Handler(Looper.myLooper() ?: Looper.getMainLooper())
+    private val timeTikRunnable = object : Runnable {
+        override fun run() {
+            val currentTime = SystemClock.uptimeMillis()
+            val nextTime = currentTime + (1000 - currentTime % 1000)
+            handler.postAtTime(this, nextTime)
+            countDownTime += 1
+            if (countDownTime % 60 == 0) {
+                onlineTime += 1
+            }
+            Log.i(TAG, "countDownTime:$countDownTime onlineTime:$onlineTime")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         registerReceiver(timeChangeBroadcastReceiver, IntentFilter().apply {
             addAction(Intent.ACTION_TIME_TICK)
         })
+        val currentTime = SystemClock.uptimeMillis()
+        val nextTime = currentTime + (1000 - currentTime % 1000)
+        handler.postAtTime(timeTikRunnable, nextTime)
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        handler.removeCallbacks(timeTikRunnable)
         unregisterReceiver(timeChangeBroadcastReceiver)
     }
 }
