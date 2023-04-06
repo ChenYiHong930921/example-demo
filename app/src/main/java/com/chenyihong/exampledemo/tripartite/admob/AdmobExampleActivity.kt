1dce23020457dcd75e194b3900a70dfee1341d1d
ChenYHong <858203471@qq.com>
2023-04-06 18:28:26 +0800

    feat(AdMob): 增加原生广告示例

--- app/src/main/java/com/chenyihong/exampledemo/tripartite/admob/AdmobExampleActivity.kt
+++ app/src/main/java/com/chenyihong/exampledemo/tripartite/admob/AdmobExampleActivity.kt
@@ -1,10 +1,20 @@
 package com.chenyihong.exampledemo.tripartite.admob
 
+import android.annotation.SuppressLint
+import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
 import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
 import android.widget.FrameLayout
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatRatingBar
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.databinding.DataBindingUtil
 import com.chenyihong.exampledemo.R
 import com.chenyihong.exampledemo.databinding.LayoutAdmobExampleActivityBinding
@@ -14,6 +24,10 @@ import com.google.android.gms.ads.initialization.InitializationStatus
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener
 import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+import com.google.android.gms.ads.nativead.MediaView
+import com.google.android.gms.ads.nativead.NativeAd
+import com.google.android.gms.ads.nativead.NativeAdOptions
+import com.google.android.gms.ads.nativead.NativeAdView
 import com.google.android.gms.ads.rewarded.RewardedAd
 import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback
 
@@ -26,7 +40,7 @@ class AdmobExampleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.layout_admob_example_activity)
-        binding.includeTitle.tvTitle.text = "AdmobExample"
+        binding.includeTitle.tvTitle.text = TAG
 
         MobileAds.initialize(this, object : OnInitializationCompleteListener {
             override fun onInitializationComplete(initializationStatus: InitializationStatus) {
@@ -44,6 +58,7 @@ class AdmobExampleActivity : AppCompatActivity() {
                     loadInterstitialAd()
                     loadRewardedVideoAd()
                     createBannerAdView()
+                    loadNativeAd()
                 }
             }
         })
@@ -51,9 +66,10 @@ class AdmobExampleActivity : AppCompatActivity() {
         binding.btnShowRewardedAd.setOnClickListener { showRewardedVideo() }
         binding.btnShowBannerAd.setOnClickListener { showBanner() }
         binding.btnHideBannerAd.setOnClickListener { hideBanner() }
+        binding.btnStopNativeAd.setOnClickListener { showChoseMuteNativeAdDialog() }
     }
 
-    //<editor-folder desc = ”interstitial ad“>
+    //<editor-folder desc = "interstitial ad">
 
     private var interstitialAd: InterstitialAd? = null
 
@@ -124,7 +140,7 @@ class AdmobExampleActivity : AppCompatActivity() {
     }
     //</editor-folder>
 
-    //<editor-folder desc = ”rewarded video ad“>
+    //<editor-folder desc = "rewarded video ad">
 
     private var rewardedAd: RewardedAd? = null
 
@@ -201,7 +217,7 @@ class AdmobExampleActivity : AppCompatActivity() {
     }
     //</editor-folder>
 
-    //<editor-folder desc = ”banner ad“>
+    // <editor-folder desc = "banner ad">
 
     private var bannerAdView: AdView? = null
 
@@ -275,4 +291,192 @@ class AdmobExampleActivity : AppCompatActivity() {
         bannerAdView?.alpha = 0f
     }
     //</editor-folder>
+
+    // <editor-folder desc = "native ad">
+
+    private var nativeAdView: NativeAdView? = null
+    private var currentNativeAd: NativeAd? = null
+    private val muteThisAdReason = ArrayList<MuteThisAdReason>()
+
+    private val adListener = object : AdListener() {
+        override fun onAdLoaded() {
+            super.onAdLoaded()
+            // 广告加载成功
+            Log.i(TAG, "nativeAd onAdLoaded")
+            nativeAdView?.let { binding.flNativeAdContainer.addView(it) }
+        }
+
+        override fun onAdFailedToLoad(loadAdError: LoadAdError) {
+            super.onAdFailedToLoad(loadAdError)
+            // 广告加载失败
+            Log.e(TAG, "nativeAd onAdFailedToLoad error:${loadAdError}")
+        }
+
+        override fun onAdOpened() {
+            super.onAdOpened()
+            // 广告页打开
+            Log.i(TAG, "nativeAd onAdOpened")
+        }
+
+        override fun onAdClicked() {
+            super.onAdClicked()
+            // 广告被点击
+            Log.i(TAG, "nativeAd onAdClicked")
+        }
+
+        override fun onAdClosed() {
+            super.onAdClosed()
+            // 广告页关闭
+            Log.i(TAG, "nativeAd onAdClosed")
+        }
+    }
+
+    private val muteListener = MuteThisAdListener {
+        // 广告关闭回调
+        Log.i(TAG, "this native ad been muted")
+    }
+
+    @SuppressLint("InflateParams")
+    private fun populateNativeAdView() {
+        currentNativeAd?.let { nativeAd ->
+            nativeAdView?.let { binding.flNativeAdContainer.removeView(it) }
+            (LayoutInflater.from(this).inflate(R.layout.layout_admob_native_ad, null) as? NativeAdView)?.run {
+                iconView = findViewById<AppCompatImageView>(R.id.iv_ad_app_icon).apply {
+                    nativeAd.icon?.let { setImageDrawable(it.drawable) }
+                    visibility = if (nativeAd.icon != null) View.VISIBLE else View.GONE
+                }
+                headlineView = findViewById<AppCompatTextView>(R.id.tv_ad_headline).apply {
+                    text = nativeAd.headline
+                }
+                advertiserView = findViewById<AppCompatTextView>(R.id.tv_advertiser).apply {
+                    text = nativeAd.advertiser
+                    visibility = if (nativeAd.advertiser != null) View.VISIBLE else View.INVISIBLE
+                }
+                starRatingView = findViewById<AppCompatRatingBar>(R.id.rb_ad_stars).apply {
+                    nativeAd.starRating?.let { rating = it.toFloat() }
+                    visibility = if (nativeAd.starRating != null) View.VISIBLE else View.INVISIBLE
+                }
+                bodyView = findViewById<AppCompatTextView>(R.id.tv_ad_body).apply {
+                    text = nativeAd.body
+                    visibility = if (nativeAd.body != null) View.VISIBLE else View.INVISIBLE
+                }
+                mediaView = findViewById<MediaView>(R.id.mv_ad_media).apply {
+                    nativeAd.mediaContent?.let {
+                        mediaContent = it
+                        it.videoController.videoLifecycleCallbacks = object : VideoController.VideoLifecycleCallbacks() {
+                            override fun onVideoStart() {
+                                super.onVideoStart()
+                                // 视频开始
+                                Log.i(TAG, "onVideoStart")
+                            }
+
+                            override fun onVideoEnd() {
+                                super.onVideoEnd()
+                                // 视频结束，结束后可以刷新广告
+                                Log.i(TAG, "onVideoEnd")
+                            }
+
+                            override fun onVideoPlay() {
+                                super.onVideoPlay()
+                                // 视频播放
+                                Log.i(TAG, "onVideoPlay")
+                            }
+
+                            override fun onVideoPause() {
+                                super.onVideoPause()
+                                // 视频暂停
+                                Log.i(TAG, "onVideoPause")
+                            }
+
+                            override fun onVideoMute(mute: Boolean) {
+                                super.onVideoMute(mute)
+                                // 视频是否静音
+                                // mute true 静音 false 非静音
+                                Log.i(TAG, "onVideoMute mute:$mute")
+                            }
+                        }
+                    }
+                }
+                callToActionView = findViewById<AppCompatButton>(R.id.btn_ad_call_to_action).apply {
+                    text = nativeAd.callToAction
+                    visibility = if (nativeAd.callToAction != null) View.VISIBLE else View.INVISIBLE
+                }
+                priceView = findViewById<AppCompatTextView>(R.id.tv_ad_price).apply {
+                    text = nativeAd.price
+                    visibility = if (nativeAd.price != null) View.VISIBLE else View.INVISIBLE
+                }
+                storeView = findViewById<AppCompatTextView>(R.id.tv_ad_store).apply {
+                    text = nativeAd.store
+                    visibility = if (nativeAd.store != null) View.VISIBLE else View.INVISIBLE
+                }
+                layoutParams = FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT).apply {
+                    gravity = Gravity.BOTTOM
+                }
+                setNativeAd(nativeAd)
+                nativeAdView = this
+            }
+        }
+    }
+
+    private fun loadNativeAd() {
+        val adLoader = AdLoader.Builder(this, "ca-app-pub-3940256099942544/2247696110")
+            .forNativeAd { nativeAd ->
+                // 如果在页面销毁后触发此回调，需要销毁NativeAd避免内存泄漏
+                if (isDestroyed || isFinishing || isChangingConfigurations) {
+                    nativeAd.destroy()
+                    return@forNativeAd
+                }
+                currentNativeAd?.destroy()
+                nativeAd.setMuteThisAdListener(muteListener)
+                currentNativeAd = nativeAd
+                // 支持自定义不再显示广告
+                if (nativeAd.isCustomMuteThisAdEnabled) {
+                    // 获取不再显示广告的原因
+                    muteThisAdReason.addAll(nativeAd.muteThisAdReasons)
+                }
+                binding.btnStopNativeAd.visibility = if (nativeAd.isCustomMuteThisAdEnabled) View.VISIBLE else View.GONE
+                populateNativeAdView()
+            }
+            .withNativeAdOptions(NativeAdOptions.Builder()
+                // 设置视频是否静音播放
+                .setVideoOptions(VideoOptions.Builder().setStartMuted(false).build())
+                // 设置自定义不再显示广告
+                .setRequestCustomMuteThisAd(true)
+                .build())
+            .withAdListener(adListener)
+            .build()
+        adLoader.loadAd(AdRequest.Builder().build())
+    }
+
+    private fun showChoseMuteNativeAdDialog() {
+        val muteThisAdReasonString = arrayOfNulls<CharSequence>(muteThisAdReason.size)
+        for ((index, item) in muteThisAdReason.withIndex()) {
+            muteThisAdReasonString[index] = item.description
+        }
+        AlertDialog.Builder(this)
+            .setTitle("关闭此原生广告的原因是？")
+            .setItems(muteThisAdReasonString) { dialog, which ->
+                if (muteThisAdReason.size > which) {
+                    muteNativeAd(muteThisAdReason[which])
+                }
+            }
+            .create()
+            .show()
+    }
+
+    private fun muteNativeAd(muteThisAdReason: MuteThisAdReason) {
+        // 可以上报用户关闭广告的原因，便于优化广告
+        currentNativeAd?.muteThisAd(muteThisAdReason)
+        nativeAdView?.destroy()
+        currentNativeAd?.destroy()
+        binding.flNativeAdContainer.removeAllViews()
+    }
+    // </editor-folder>
+
+    override fun onDestroy() {
+        super.onDestroy()
+        hideBanner()
+        bannerAdView?.destroy()
+        currentNativeAd?.destroy()
+    }
 }
