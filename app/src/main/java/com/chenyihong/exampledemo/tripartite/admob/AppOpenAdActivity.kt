f6532bab2cf6e8dfbd15efa6f43c28b3e3fb4adb
ChenYHong <858203471@qq.com>
2023-04-07 11:38:22 +0800

    refactor(AdMob): 去除无用的导包

--- app/src/main/java/com/chenyihong/exampledemo/tripartite/admob/AppOpenAdActivity.kt
+++ app/src/main/java/com/chenyihong/exampledemo/tripartite/admob/AppOpenAdActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.chenyihong.exampledemo.R
 import com.chenyihong.exampledemo.base.ExampleApplication
