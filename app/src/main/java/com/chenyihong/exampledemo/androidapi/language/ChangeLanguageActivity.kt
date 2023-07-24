a3810ada52b3d2818da1ec49f0691c712eaaab2b
ChenYHong <858203471@qq.com>
2023-07-24 16:05:46 +0800

    refactor(Language): 优化首选语言API示例代码

--- app/src/main/java/com/chenyihong/exampledemo/androidapi/language/ChangeLanguageActivity.kt
+++ app/src/main/java/com/chenyihong/exampledemo/androidapi/language/ChangeLanguageActivity.kt
@@ -7,16 +7,15 @@ import android.view.LayoutInflater
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.os.LocaleListCompat
+import com.chenyihong.exampledemo.R
 import com.chenyihong.exampledemo.androidapi.gesturedetector.BaseGestureDetectorActivity
 import com.chenyihong.exampledemo.databinding.LayoutChangeLanguageActivityBinding
+import org.xmlpull.v1.XmlPullParser
 import java.util.Locale
 
 class ChangeLanguageActivity : BaseGestureDetectorActivity<LayoutChangeLanguageActivityBinding>() {
 
-    private val availableLanguage = arrayOf("zh", "en", "ru", "ko", "ja")
-    private val availableLanguageDisplayName = arrayOf("Chinese", "English", "Russian", "korean", "Japanese")
-
-    private var currentLanguage = "zh"
+    private var currentLanguage = ""
 
     override fun initViewBinding(layoutInflater: LayoutInflater): LayoutChangeLanguageActivityBinding {
         return LayoutChangeLanguageActivityBinding.inflate(layoutInflater)
@@ -31,13 +30,29 @@ class ChangeLanguageActivity : BaseGestureDetectorActivity<LayoutChangeLanguageA
         } else {
             AppCompatDelegate.getApplicationLocales()[0]?.toLanguageTag() ?: ""
         }
+        val supportLanguageTag = ArrayList<String>()
+        val supportLanguageDisplayName = ArrayList<String>()
+        resources.getXml(R.xml.locale_config).run {
+            var eventType = eventType
+            while (XmlPullParser.END_DOCUMENT != eventType) {
+                if (XmlPullParser.START_TAG == eventType) {
+                    if ("locale" == name) {
+                        val languageTag = getAttributeValue(0)
+                        supportLanguageTag.add(languageTag)
+                        supportLanguageDisplayName.add(Locale.forLanguageTag(languageTag).getDisplayName(Locale.forLanguageTag(currentLanguage)))
+                    }
+                }
+                eventType = next()
+            }
+        }
+        Log.i("-,-,-", "init supportLanguageDisplayName:$supportLanguageDisplayName")
         binding.includeTitle.tvTitle.text = "Change Language Example"
         binding.btnChangeLanguage.setOnClickListener {
-            val currentIndex = availableLanguage.indexOf(currentLanguage)
+            val currentIndex = supportLanguageTag.indexOf(supportLanguageTag.find { currentLanguage.contains(it) })
             AlertDialog.Builder(this)
                 .setTitle("Chose Language")
-                .setSingleChoiceItems(availableLanguageDisplayName, if (currentIndex == -1) 0 else currentIndex) { _, which ->
-                    currentLanguage = availableLanguage[which]
+                .setSingleChoiceItems(supportLanguageDisplayName.toArray(emptyArray()), if (currentIndex == -1) 0 else currentIndex) { _, which ->
+                    currentLanguage = supportLanguageTag[which]
                 }
                 .setPositiveButton("ok") { dialog, _ ->
                     // 切换语言
