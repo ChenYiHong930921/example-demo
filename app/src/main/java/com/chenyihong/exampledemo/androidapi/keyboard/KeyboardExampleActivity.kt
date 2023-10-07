fb430b750358d7c79490c947c1ec0d99ed7a0b82
ChenYHong <858203471@qq.com>
2023-10-07 18:06:36 +0800

    refactor(Keyboard): 调整软键盘处理示例
    1. 增加示例代码。
    2. 增加注释。

--- app/src/main/java/com/chenyihong/exampledemo/androidapi/keyboard/KeyboardExampleActivity.kt
+++ app/src/main/java/com/chenyihong/exampledemo/androidapi/keyboard/KeyboardExampleActivity.kt
@@ -1,5 +1,6 @@
 package com.chenyihong.exampledemo.androidapi.keyboard
 
+import android.annotation.SuppressLint
 import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -22,12 +23,15 @@ class KeyboardExampleActivity : BaseGestureDetectorActivity<LayoutKeyboardExampl
         return LayoutKeyboardExampleActivityBinding.inflate(layoutInflater)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN)
         super.onCreate(savedInstanceState)
         ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { _, windowInsets ->
+            // 当窗口发生变化时，回调会被执行（例如系统状态栏变化、软键盘变化）
             windowInsets.getInsets(WindowInsetsCompat.Type.ime()).run {
-                // 当软键盘弹出时，bottom为软件的高度
+                // 当软键盘显示时，bottom的值为软键盘的高度加导航栏的高度。软键盘隐藏时，bottom的值为0。
+                binding.tvKeyboardHeight.text = "keyboard height: $bottom"
                 keyboardShown = bottom > 0
                 // 处理输入框被软键盘遮住的问题
                 handleKeyboardObscuringView()
@@ -66,16 +70,21 @@ class KeyboardExampleActivity : BaseGestureDetectorActivity<LayoutKeyboardExampl
         if (keyboardShown) {
             currentFocus?.let { focusView ->
                 val rect = Rect()
+                // 获取当前窗口的可见区域相对于页面根布局的位置和尺寸
                 binding.root.getWindowVisibleDisplayFrame(rect)
                 val locations = IntArray(2)
+                // 获取当前焦点控件在页面中的位置
                 focusView.getLocationInWindow(locations)
+                // 当前窗口可见区域的底部高度小于焦点控件的y轴坐标加上它的高度时
+                // 判定为软键盘已经遮挡住了控件
                 if (rect.bottom < locations[1] + focusView.height) {
-                    // 如果输入框被软件盘遮挡，则滚动页面至可以显示输入框的位置
+                    // 如果输入框被软件盘遮挡，则滚动页面至可以完全显示输入框的位置
                     binding.root.scrollTo(0, (locations[1] + focusView.height + focusView.height / 2) - rect.bottom)
                     needRestore = true
                 }
             }
         } else if (needRestore) {
+            // 需要时回滚到页面的顶部
             binding.root.scrollTo(0, 0)
         }
     }
