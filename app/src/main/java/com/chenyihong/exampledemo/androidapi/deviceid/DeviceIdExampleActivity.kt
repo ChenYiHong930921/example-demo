7d9353e055db33c5857f0ff13c8a9ff104e3ba0a
ChenYHong <858203471@qq.com>
2024-09-24 15:23:21 +0800

    refactor(DeviceId API): 调整获取设备标识API示例代码

--- app/src/main/java/com/chenyihong/exampledemo/androidapi/deviceid/DeviceIdExampleActivity.kt
+++ app/src/main/java/com/chenyihong/exampledemo/androidapi/deviceid/DeviceIdExampleActivity.kt
@@ -15,60 +15,40 @@ import java.net.NetworkInterface
 
 class DeviceIdExampleActivity : AppCompatActivity() {
 
-    private val singlePermissionRequestLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {}
+    private lateinit var binding: LayoutDeviceIdExampleActivityBinding
+
+    private val deviceIdPermissionRequestLauncher = registerForActivityResult(/* contract = */ ActivityResultContracts.RequestPermission()) {
+        // 无论是否授权，都进行获取，作对比
+        getDeviceID()
+    }
+
+    private val serialPermissionRequestLauncher = registerForActivityResult(/* contract = */ ActivityResultContracts.RequestPermission()) {
+        // 无论是否授权，都进行获取，作对比
+        getSerial()
+    }
 
     @SuppressLint("HardwareIds", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = LayoutDeviceIdExampleActivityBinding.inflate(layoutInflater).also {
+        binding = LayoutDeviceIdExampleActivityBinding.inflate(layoutInflater).also {
             setContentView(it.root)
         }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-            singlePermissionRequestLauncher.launch(Manifest.permission.READ_PHONE_STATE)
-        }
-        val telephonyManager = getSystemService(TelephonyManager::class.java)
+
         binding.btnGetDeviceId.setOnClickListener {
-            val deviceIDStrBuilder = StringBuilder()
-            try {
-                deviceIDStrBuilder.append("deviceId:").append(telephonyManager.deviceId)
-            } catch (e: Exception) {
-                e.printStackTrace()
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                deviceIdPermissionRequestLauncher.launch(Manifest.permission.READ_PHONE_STATE)
+            } else {
+                getDeviceID()
             }
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                try {
-                    deviceIDStrBuilder.append("\nimei:").append(telephonyManager.imei)
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                }
-                try {
-                    deviceIDStrBuilder.append("\nmeid:").append(telephonyManager.meid)
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                }
-            }
-            binding.tvResultValue.text = deviceIDStrBuilder
         }
         binding.btnGetAndroidId.setOnClickListener {
             binding.tvResultValue.text = "ANDROID_ID:${Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)}"
         }
         binding.btnGetDeviceSerial.setOnClickListener {
-            // 设备序列号 10.0开始无法获取
-            var deviceSerial = ""
-            try {
-                Build.SERIAL?.takeIf { !it.equals("UNKNOWN", true) }?.let { deviceSerial = it }
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-            try {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    deviceSerial = Build.getSerial()
-                }
-            } catch (e: Exception) {
-                e.printStackTrace()
-                binding.tvResultValue.text = "get device serial failed due to ${e.message}"
-            }
-            if (deviceSerial.isNotEmpty()) {
-                binding.tvResultValue.text = "deviceSerial:$deviceSerial"
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                serialPermissionRequestLauncher.launch(Manifest.permission.READ_PHONE_STATE)
+            } else {
+                getSerial()
             }
         }
         binding.btnGetMacAddress.setOnClickListener {
@@ -81,5 +61,48 @@ class DeviceIdExampleActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    private fun getDeviceID() {
+        val telephonyManager = getSystemService(TelephonyManager::class.java)
+        val deviceIDStrBuilder = StringBuilder()
+        try {
+            deviceIDStrBuilder.append("deviceId:").append(telephonyManager.deviceId)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            try {
+                // 设备类型为GSM且有权限获取时不为空
+                deviceIDStrBuilder.append("\nIMEI:").append(telephonyManager.imei)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            try {
+                // 设备类型为CDMA且有权限获取时不为空
+                deviceIDStrBuilder.append("\nMEID:").append(telephonyManager.meid)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+        binding.tvResultValue.text = deviceIDStrBuilder
+    }
+
+    private fun getSerial() {
+        val serialStrBuilder = StringBuilder()
+        try {
+            serialStrBuilder.append("Build.SERIAL:").append(Build.SERIAL)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                serialStrBuilder.append("\nBuild.getSerial():").append(Build.getSerial())
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+
+        }
+        binding.tvResultValue.text = serialStrBuilder
     }
 }
